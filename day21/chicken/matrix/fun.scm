fd2241083db6388cde0293937532a4a7c7e739a0
terry <terry.cadd@gmail.com>
2024-05-11 17:22:58 +0100

    try a two hash method for matrices

---
+++ day21/chicken/matrix/fun.scm
@@ -0,0 +1,25 @@
+
+
+
+matrix in lisp
+hash table then use key (1 1) say
+
+if matrix width is divisble by 2
+2 4 6 8 10 12 ...
+break matrix up into 2 x 2 squares converted to 3x3 squares
+think of 2 x 2 squares being already in place
+insert blank rows and blank columns every nth row ,
+
+keep track of largest element set so far ?
+or do we just do a copy of matrix (a hash) and leave blank spaces #f falsies
+in place
+
+show matrix facility row 1 , col 1 .. N
+... ron N , col 1 ... N
+matrices are always square
+
+if we do check and replace 2x2 by 3x3 when matrix has been expanded ( post expansion )
+then the 3x3 matrix can then be slotted in place
+
+
+
